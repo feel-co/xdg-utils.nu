e733a932144474ff91b48e36ba743093d8b5c88f
Amaan Qureshi <git@amaanq.com>
2026-08-21 21:30:45 -0400

    deps: bump Nushell plugins to 0.115.0

--- flake.nix
+++ flake.nix
@@ -1,95 +1,129 @@
 {
   inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-    forAllSystems = lib.genAttrs lib.platforms.linux;
-    pkgsFor = system: nixpkgs.legacyPackages.${system} or (import nixpkgs {inherit system;});
-  in {
-    overlays = {
-      default = final: _: let
-        workspaceSrc = lib.fileset.toSource {
-          root = ./plugins;
-          fileset = lib.fileset.unions [
-            ./plugins/Cargo.toml
-            ./plugins/Cargo.lock
-            ./plugins/xdg-core
-            ./plugins/nu_plugin_xdg
-            ./plugins/nu_plugin_dbus
-          ];
-        };
-        nu_plugin_dbus = final.rustPlatform.buildRustPackage {
-          pname = "nu_plugin_dbus";
-          version = "0.1.0";
-          src = workspaceSrc;
-          cargoLock.lockFile = ./plugins/Cargo.lock;
-          cargoBuildFlags = ["-p" "nu_plugin_dbus"];
-          cargoTestFlags = ["-p" "nu_plugin_dbus"];
-          nativeBuildInputs = [final.pkg-config];
-          buildInputs = [final.dbus];
-          meta.mainProgram = "nu_plugin_dbus";
-        };
-        nu_plugin_xdg = final.rustPlatform.buildRustPackage {
-          pname = "nu_plugin_xdg";
-          version = "0.1.0";
-          src = workspaceSrc;
-          cargoLock.lockFile = ./plugins/Cargo.lock;
-          cargoBuildFlags = ["-p" "nu_plugin_xdg"];
-          cargoTestFlags = ["-p" "nu_plugin_xdg" "-p" "xdg-core"];
-          meta.mainProgram = "nu_plugin_xdg";
-        };
-        mk = coreutils:
-          final.callPackage ./nix/package.nix {
-            inherit coreutils nu_plugin_dbus nu_plugin_xdg;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.platforms.linux;
+      pkgsFor = system: nixpkgs.legacyPackages.${system} or (import nixpkgs { inherit system; });
+    in
+    {
+      overlays = {
+        default =
+          final: _:
+          let
+            workspaceSrc = lib.fileset.toSource {
+              root = ./plugins;
+              fileset = lib.fileset.unions [
+                ./plugins/Cargo.toml
+                ./plugins/Cargo.lock
+                ./plugins/xdg-core
+                ./plugins/nu_plugin_xdg
+                ./plugins/nu_plugin_dbus
+              ];
+            };
+            nu_plugin_dbus = final.rustPlatform.buildRustPackage {
+              pname = "nu_plugin_dbus";
+              version = "0.1.0";
+              src = workspaceSrc;
+              cargoLock.lockFile = ./plugins/Cargo.lock;
+              cargoBuildFlags = [
+                "-p"
+                "nu_plugin_dbus"
+              ];
+              cargoTestFlags = [
+                "-p"
+                "nu_plugin_dbus"
+              ];
+              nativeBuildInputs = [ final.pkg-config ];
+              buildInputs = [ final.dbus ];
+              meta.mainProgram = "nu_plugin_dbus";
+            };
+            nu_plugin_xdg = final.rustPlatform.buildRustPackage {
+              pname = "nu_plugin_xdg";
+              version = "0.1.0";
+              src = workspaceSrc;
+              cargoLock.lockFile = ./plugins/Cargo.lock;
+              cargoBuildFlags = [
+                "-p"
+                "nu_plugin_xdg"
+              ];
+              cargoTestFlags = [
+                "-p"
+                "nu_plugin_xdg"
+                "-p"
+                "xdg-core"
+              ];
+              meta.mainProgram = "nu_plugin_xdg";
+            };
+            mk =
+              coreutils:
+              final.callPackage ./nix/package.nix {
+                inherit coreutils nu_plugin_dbus nu_plugin_xdg;
+              };
+          in
+          {
+            inherit nu_plugin_dbus nu_plugin_xdg;
+            xdg-utils-nu = mk final.coreutils;
+            xdg-utils-nu-uutils = mk final.uutils-coreutils-noprefix;
           };
-      in {
-        inherit nu_plugin_dbus nu_plugin_xdg;
-        xdg-utils-nu = mk final.coreutils;
-        xdg-utils-nu-uutils = mk final.uutils-coreutils-noprefix;
       };
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = (pkgsFor system).extend self.overlays.default;
+        in
+        {
+          inherit (pkgs)
+            xdg-utils-nu
+            xdg-utils-nu-uutils
+            nu_plugin_dbus
+            nu_plugin_xdg
+            ;
+          default = pkgs.xdg-utils-nu;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "xdg-utils-nu";
+
+            strictDeps = true;
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.nufmt
+              (pkgs.writers.writeNuBin "nucheck" /* nu */ ''
+                glob $"($env.out)/bin/.xdg-*-wrapped"
+                | each {|wrapped|
+                    let diags = (
+                      nu --ide-check 100 $wrapped
+                        | lines
+                        | each {|l| try { $l | from json } catch { null } }
+                        | where {|d| $d != null and $d.type == "diagnostic" }
+                    )
+                    if not ($diags | is-empty) {
+                      print --stderr $"Parse errors in ($wrapped):"
+                      $diags | each {|d| print --stderr ($d | to json --raw) }
+                      exit 1
+                    }
+                  }
+                | ignore
+              '')
+            ];
+            buildInputs = [ pkgs.dbus ];
+          };
+        }
+      );
     };
-
-    packages = forAllSystems (system: let
-      pkgs = (pkgsFor system).extend self.overlays.default;
-    in {
-      inherit (pkgs) xdg-utils-nu xdg-utils-nu-uutils nu_plugin_dbus nu_plugin_xdg;
-      default = pkgs.xdg-utils-nu;
-    });
-
-    devShells = forAllSystems (system: let
-      pkgs = pkgsFor system;
-    in {
-      default = pkgs.mkShell {
-        name = "xdg-utils-nu";
-
-        strictDeps = true;
-        nativeBuildInputs = [
-          pkgs.pkg-config
-          pkgs.nufmt
-          (pkgs.writers.writeNuBin "nucheck" /* nu */ ''
-            glob $"($env.out)/bin/.xdg-*-wrapped"
-            | each {|wrapped|
-                let diags = (
-                  nu --ide-check 100 $wrapped
-                    | lines
-                    | each {|l| try { $l | from json } catch { null } }
-                    | where {|d| $d != null and $d.type == "diagnostic" }
-                )
-                if not ($diags | is-empty) {
-                  print --stderr $"Parse errors in ($wrapped):"
-                  $diags | each {|d| print --stderr ($d | to json --raw) }
-                  exit 1
-                }
-              }
-            | ignore
-          '')
-        ];
-        buildInputs = [pkgs.dbus];
-      };
-    });
-  };
 }
--- nix/package.nix
+++ nix/package.nix
@@ -7,7 +7,8 @@
   glib,
   nu_plugin_dbus,
   nu_plugin_xdg,
-}: let
+}:
+let
   runtimeDeps = [
     coreutils
     glib.bin
@@ -21,94 +22,103 @@
     "${nu_plugin_xdg}/bin/nu_plugin_xdg"
   ];
 in
-  stdenvNoCC.mkDerivation {
-    pname = "xdg-utils";
-    version = "1.2.1";
+stdenvNoCC.mkDerivation {
+  pname = "xdg-utils";
+  version = "1.2.1";
 
-    src = let
+  src =
+    let
       fs = lib.fileset;
       s = ../.;
     in
-      fs.toSource {
-        root = s;
-        fileset = fs.unions [
-          (fs.fileFilter (file: builtins.any file.hasExt ["nu"]) (s + /scripts))
-        ];
-      };
-
-    nativeBuildInputs = [makeWrapper nushell];
-
-    doInstallCheck = true;
-    installCheckInputs = [nushell];
-    installCheckPhase = ''
-      runHook preInstallCheck
-
-      # Parse-check every installed script with the plugin registry loaded
-      nu --plugin-config $out/share/nu-plugins/registry.msgpackz --commands '
-        glob $"($env.out)/libexec/xdg-utils-nu/xdg-*"
-          | where {|s| ($s | path basename) != "xdg-utils-common.nu" }
-          | each {|script|
-              let diags = (
-                nu --plugin-config $"($env.out)/share/nu-plugins/registry.msgpackz" --ide-check 100 $script
-                  | lines
-                  | each {|l| try { $l | from json } catch { null } }
-                  | where {|d| $d != null and $d.type == "diagnostic" }
-              )
-              if not ($diags | is-empty) {
-                print --stderr $"Parse errors in ($script):"
-                $diags | each {|d| print --stderr ($d | to json --raw) }
-                exit 1
-              }
-            }
-          | ignore
-      '
-
-      runHook postInstallCheck
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin $out/libexec/xdg-utils-nu $out/share/nu-plugins
-
-      # Install Nushell scripts to libexec.
-      for f in scripts/xdg-*.nu; do
-        base=$(basename "$f" .nu)
-        # Keep .nu extension on the common module for `use` resolution.
-        if [ "$base" = "xdg-utils-common" ]; then
-          cp "$f" "$out/libexec/xdg-utils-nu/xdg-utils-common.nu"
-        else
-          cp "$f" "$out/libexec/xdg-utils-nu/$base"
-        fi
-      done
-
-      # Build a plugin registry pointing at the nix-store plugin binaries.
-      export HOME=$TMPDIR
-      reg=$out/share/nu-plugins/registry.msgpackz
-      for plugin in ${lib.escapeShellArgs plugins}; do
-        nu --plugin-config "$reg" --commands "plugin add $plugin"
-      done
-
-      # Keep uncompressed plugin store references in each wrapper since Nix cannot
-      # discover them inside the compressed registry when computing the closure.
-      pluginPath="${lib.makeBinPath [nu_plugin_dbus nu_plugin_xdg]}"
-      for f in $out/libexec/xdg-utils-nu/xdg-*; do
-        base=$(basename "$f")
-        [ "$base" = "xdg-utils-common.nu" ] && continue
-        makeWrapper ${nushell}/bin/nu "$out/bin/$base" \
-          --add-flags "--plugin-config $reg" \
-          --add-flags "$f" \
-          --prefix PATH ":" "$out/bin:${lib.makeBinPath runtimeDeps}:$pluginPath"
-      done
-
-      runHook postInstall
-    '';
-
-    meta = {
-      homepage = "https://www.freedesktop.org/wiki/Software/xdg-utils/";
-      description = "Set of command line tools that assist applications with a variety of desktop integration tasks";
-      license = lib.licenses.mit;
-      maintainers = [];
-      platforms = lib.platforms.linux;
+    fs.toSource {
+      root = s;
+      fileset = fs.unions [
+        (fs.fileFilter (file: builtins.any file.hasExt [ "nu" ]) (s + /scripts))
+      ];
     };
-  }
+
+  nativeBuildInputs = [
+    makeWrapper
+    nushell
+  ];
+
+  doInstallCheck = true;
+  installCheckInputs = [ nushell ];
+  installCheckPhase = ''
+    runHook preInstallCheck
+
+    # Parse-check every installed script with the plugin registry loaded
+    nu --plugin-config $out/share/nu-plugins/registry.msgpackz --commands '
+      glob $"($env.out)/libexec/xdg-utils-nu/xdg-*"
+        | where {|s| ($s | path basename) != "xdg-utils-common.nu" }
+        | each {|script|
+            let diags = (
+              nu --plugin-config $"($env.out)/share/nu-plugins/registry.msgpackz" --ide-check 100 $script
+                | lines
+                | each {|l| try { $l | from json } catch { null } }
+                | where {|d| $d != null and $d.type == "diagnostic" }
+            )
+            if not ($diags | is-empty) {
+              print --stderr $"Parse errors in ($script):"
+              $diags | each {|d| print --stderr ($d | to json --raw) }
+              exit 1
+            }
+          }
+        | ignore
+    '
+
+    runHook postInstallCheck
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/libexec/xdg-utils-nu $out/share/nu-plugins
+
+    # Install Nushell scripts to libexec.
+    for f in scripts/xdg-*.nu; do
+      base=$(basename "$f" .nu)
+      # Keep .nu extension on the common module for `use` resolution.
+      if [ "$base" = "xdg-utils-common" ]; then
+        cp "$f" "$out/libexec/xdg-utils-nu/xdg-utils-common.nu"
+      else
+        cp "$f" "$out/libexec/xdg-utils-nu/$base"
+      fi
+    done
+
+    # Build a plugin registry pointing at the nix-store plugin binaries.
+    export HOME=$TMPDIR
+    reg=$out/share/nu-plugins/registry.msgpackz
+    for plugin in ${lib.escapeShellArgs plugins}; do
+      nu --plugin-config "$reg" --commands "plugin add $plugin"
+    done
+
+    # Keep uncompressed plugin store references in each wrapper since Nix cannot
+    # discover them inside the compressed registry when computing the closure.
+    pluginPath="${
+      lib.makeBinPath [
+        nu_plugin_dbus
+        nu_plugin_xdg
+      ]
+    }"
+    for f in $out/libexec/xdg-utils-nu/xdg-*; do
+      base=$(basename "$f")
+      [ "$base" = "xdg-utils-common.nu" ] && continue
+      makeWrapper ${nushell}/bin/nu "$out/bin/$base" \
+        --add-flags "--plugin-config $reg" \
+        --add-flags "$f" \
+        --prefix PATH ":" "$out/bin:${lib.makeBinPath runtimeDeps}:$pluginPath"
+    done
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://www.freedesktop.org/wiki/Software/xdg-utils/";
+    description = "Set of command line tools that assist applications with a variety of desktop integration tasks";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+}
